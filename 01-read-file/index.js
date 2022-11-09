8efe51221982ef7ed49d4991eb67fdcc19595481
Liliya Belkova <104428837+Lili-126@users.noreply.github.com>
2022-11-10 00:57:18 +0500

    Create index.js

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const textFile = path.join(__dirname, 'index', 'text');
+const stream = fs.createReadStream('text.txt', 'utf-8');
+let data = '';
+stream.on('data', chunk => data += chank);
+
+const {stdout} = process;
+stout.write(data);
